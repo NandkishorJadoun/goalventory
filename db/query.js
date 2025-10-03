bdf0f0e1e1b5ef9eaf3baa244419aedd7cdec9e3
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-10-03 16:12:53 +0530

    feat: add queries for getting items from db

--- db/query.js
+++ db/query.js
@@ -1,10 +1,21 @@
 const pool = require("./pool");
 
 async function getAllPlayers(req, res) {
-  const { rows } = await pool.query("SELECT * FROM players");
+  const { rows } = await pool.query(
+    `SELECT players.id, players.player_name, position_name, league_name
+    FROM players 
+    JOIN positions ON players.position_id = positions.id 
+    JOIN leagues ON players.league_id = leagues.id`,
+  );
+  return rows;
+}
+
+async function getAllPositions(req, res) {
+  const { rows } = await pool.query(`SELECT position_name FROM positions`);
   return rows;
 }
 
 module.exports = {
   getAllPlayers,
+  getAllPositions,
 };
