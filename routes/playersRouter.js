4ce7555cd924be98f281eecd923cb694be38d5db
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-10-03 15:02:25 +0530

    feat: add player routers that list all the players

---
+++ routes/playersRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const playersController = require("../controllers/playerController");
+
+const playersRouter = Router();
+
+playersRouter.get("/", playersController.getAllPlayers);
+
+module.exports = playersRouter;
